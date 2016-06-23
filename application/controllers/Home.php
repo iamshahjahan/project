a4b6bbb78253fa88ed8c6cee9773bc69cd252860
hammad7 <hammad73m@gmail.com>
2016-06-23 17:58:04 +0530

    login...continue

---
+++ application/controllers/Home.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Home extends CI_Controller {
+ 
+ function __construct()
+ {
+   parent::__construct();
+   
+ }
+ 
+ function index()
+ {
+   if($this->session->userdata('logged_in'))
+   {
+     $session_data = $this->session->userdata('logged_in');
+     $data['email'] = $session_data['email'];
+     $this->load->view('home_view', $data);
+   }
+   else
+   {
+     //If no session, redirect to login page
+     redirect('login', 'refresh');
+   }
+ }
+ 
+ function logout()
+ {
+   $this->session->unset_userdata('logged_in');
+   session_destroy();
+   redirect('home', 'refresh');
+ }
+ 
+}
+ 
+?>
