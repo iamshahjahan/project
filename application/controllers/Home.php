21fa7dae5e4865000373c6f15c21b4b5ffc8d90d
hammad7 <hammad73m@gmail.com>
2016-06-24 10:43:43 +0530

    login integrattion with connection-pdo

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,16 +4,19 @@ class Home extends CI_Controller {
  function __construct()
  {
    parent::__construct();
+   $this->load->helper('url');
+   $this->load->model('Users');
    
  }
  
  function index()
  {
+ 	print_r($this->Users->get());//shahjehan's
    if($this->session->userdata('logged_in'))
    {
      $session_data = $this->session->userdata('logged_in');
      $data['email'] = $session_data['email'];
-     $this->load->view('home_view', $data);
+     $this->load->view('home_view', $data);//send sess data
    }
    else
    {
@@ -35,23 +38,23 @@ class Home extends CI_Controller {
 /**
 	* This home controller does the tasks related to home page on the website.
 	*/
-	class Home extends CI_Controller
-	{
+	// class Home extends CI_Controller
+	// {
 		
-		function __construct()
-		{
-			parent::__construct();
-			//load the pdo db config 
-			// $this->pdo = $this->load->database('pdo', true);
-			$this->load->helper('url');
-			$this->load->model('Users');
-		}
+	// 	function __construct()
+	// 	{
+	// 		parent::__construct();
+	// 		//load the pdo db config 
+	// 		// $this->pdo = $this->load->database('pdo', true);
+	// 		$this->load->helper('url');
+	// 		$this->load->model('Users');
+	// 	}
 
-		function index()
-		{
-			print_r($this->Users->get());			
-		}
+	// 	function index()
+	// 	{
+	// 		print_r($this->Users->get());			
+	// 	}
 
 		
-	}
+	// }
 ?>
